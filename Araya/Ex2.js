b70dd6c3d434555778ec8e58291d7e16d6f8f702
Gabgf10 <160678956+Gabgf10@users.noreply.github.com>
2024-11-18 22:57:17 -0300

    Add files via upload

---
+++ Araya/Ex2.js
@@ -0,0 +1,9 @@
+const nome = prompt("Olá usuário, qual é o seu nome?");
+
+const idade = prompt("Quantos anos você tem?");
+
+const linguagem = prompt("Qual é sua linguagem de programação preferida?");
+
+const msg = `"Olá ${nome}, você tem ${idade} anos e sua linguagem preferida é ${linguagem}!"!`;
+
+alert(msg);
